7b225d7fd35f60bfe0cfce4f603d50ad46832d79
Tom Vander Aa <vanderaa@imec.be>
2024-06-11 10:47:01 +0200

    __init__.py: use importlib for version

--- h5sparse/__init__.py
+++ h5sparse/__init__.py
@@ -1,4 +1,4 @@
-import pkg_resources
 from .h5sparse import Group, File, Dataset, get_format_str  # noqa: F401
 
-__version__ = pkg_resources.get_distribution("h5sparse-tensor").version
+from importlib.metadata import version
+__version__ = version('h5sparse-tensor')
